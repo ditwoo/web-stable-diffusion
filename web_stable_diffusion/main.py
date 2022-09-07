0c5e81d84d7685b2224a09e956ab151faa689421
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2022-09-07 20:17:38 +0300

    fix: streamlit page

--- web_stable_diffusion/main.py
+++ web_stable_diffusion/main.py
@@ -1,13 +1,11 @@
-import time
-
 import torch
-import numpy as np
+import tabulate
 import streamlit as st
-from PIL import Image
 
 from diffuse import CustomDiffusionPipeline, generate_image
 
 
+@st.cache(allow_output_mutation=True)
 def init_pipeline(device, fp16=False, auth=True):
     kwargs = {}
     model_id = "CompVis/stable-diffusion-v1-4"
@@ -23,12 +21,7 @@ def init_pipeline(device, fp16=False, auth=True):
     return pipe
 
 
-# inference parameters
-DEVICE = "cpu" if not torch.cuda.is_available() else "cuda"
-PIPE = init_pipeline(DEVICE, fp16=False, auth=True)
-
-
-def main():
+def main(device, pipe):
     with st.form(key="request"):
         st.write(
             """
@@ -37,7 +30,7 @@ def main():
     """
         )
 
-        prompt = st.text_area("Prompt", value="cat with a red hat in a forest", key="prompt")
+        prompt = st.text_area("Prompt", value="", key="prompt")
         left_column, right_column = st.columns(2)
         with left_column:
             iterations = int(st.text_input("Iterations", value=50, key="iterations"))
@@ -47,10 +40,13 @@ def main():
         st.form_submit_button("Submit")
 
         if prompt:
-            print(prompt, iterations, seed, nsfw)
-            image = generate_image(PIPE, DEVICE, prompt=prompt, iterations=iterations, initial_seed=seed, nsfw=nsfw)
+            info = [("Device", device), ("Prompt", prompt), ("Iterations", iterations), ("Seed", seed), ("NSFW", nsfw)]
+            print(tabulate.tabulate(info))
+            image = generate_image(pipe, device, prompt=prompt, iterations=iterations, initial_seed=seed, nsfw=nsfw)
             st.image(image, use_column_width=True)
 
 
 if __name__ == "__main__":
-    main()
+    device = "cpu" if not torch.cuda.is_available() else "cuda"
+    pipeline = init_pipeline(device, fp16=False, auth=True)
+    main(device, pipeline)
